1eaf8174d58def9216fa702b3cdb23f31dab2e53
agalotaibi <hopes.gmo@gmail.com>
2024-10-20 11:05:29 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Amaal on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
